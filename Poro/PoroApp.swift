1bbb408d9c340f8cd8e7e4c4d092dcf2c47f4eaf
Andrew Vong <andrewvong426@gmail.com>
2026-01-15 10:00:00 -0600

    Initial Commit

---
+++ Poro/PoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoroApp.swift
+//  Poro
+//
+//  Created by Andrew Vong on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct PoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
