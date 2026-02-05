49d2d2ec05d0f584836f049069703c3cd1222503
Andrew Vong <andrewvong426@gmail.com>
2026-02-05 10:00:00 -0600

    switch off menubar to NSPanel

--- Poro/AppDelegate.swift
+++ Poro/AppDelegate.swift
@@ -2,9 +2,7 @@ import AppKit
 //Object getting lifecycle callbacks from macOS
 //Allows us to respond to OS level events such as app launching, closing ,etc
 final class AppDelegate: NSObject, NSApplicationDelegate {
-    private var statusItemController: StatusItemController?
-
     func applicationDidFinishLaunching(_ notification: Notification) {
-        statusItemController = StatusItemController()
+        // The assistant window controller will be created here in the next step.
     }
 }
